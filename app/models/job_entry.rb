b2196ee2115d8f5f3967121d28a6fde33d1d7c5e
Alex Kochurov <alex.kochurov@gmail.com>
2018-11-19 19:31:36 +0300

    Annotate

--- app/models/job_entry.rb
+++ app/models/job_entry.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: job_entries
+#
+#  id               :bigint(8)        not null, primary key
+#  title            :string           not null
+#  url              :string           not null
+#  company          :string
+#  company_url      :string
+#  location         :string
+#  remote           :boolean          default(FALSE)
+#  job_type         :integer
+#  description      :text
+#  experience_level :integer
+#  tags             :text             default([]), is an Array
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class JobEntry < ApplicationRecord
 end
--- test/models/job_entry_test.rb
+++ test/models/job_entry_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: job_entries
+#
+#  id               :bigint(8)        not null, primary key
+#  title            :string           not null
+#  url              :string           not null
+#  company          :string
+#  company_url      :string
+#  location         :string
+#  remote           :boolean          default(FALSE)
+#  job_type         :integer
+#  description      :text
+#  experience_level :integer
+#  tags             :text             default([]), is an Array
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 require 'test_helper'
 
 class JobEntryTest < ActiveSupport::TestCase
